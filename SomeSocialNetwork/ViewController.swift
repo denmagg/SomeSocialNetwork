d992fd9c26cdea1bb0ff56a532a4d0c5bc01d4c3
Denis <denmagg.work@gmail.com>
2022-06-28 14:04:20 +0300

    Initial Commit

---
+++ SomeSocialNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomeSocialNetwork
+//
+//  Created by Денис Медведев on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
